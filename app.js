0cdaf6e9a88c852ae9330a2136c2619022af0038
millath123 <adilamillath@gmail.com>
2024-02-17 20:15:47 +0530

    user profile change

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use(userRoutes);
 
 const port = process.env.PORT || '8080';
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // console.log(`Server is running on port ${port}`);
 });
